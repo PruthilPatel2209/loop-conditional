3015b44302af16916d9e2fd2040aabf5a98f4929
PruthilPatel2209 <pruthil2006@gmail.com>
2025-12-15 15:21:24 +0530

    Initial commit

---
+++ src/Primechecker.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Primechecker {
+    static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        boolean ans = isPrime(n);
+        System.out.println(ans);
+    }
+
+    static boolean isPrime(int k){
+
+        if(k < 1)
+            return false;
+
+        int c = 2;
+        while( c*c <= k){
+
+            if(k%c == 0){
+                return false;
+            }
+            c++;
+        }
+
+        return c*c > k;
+    }
+}
